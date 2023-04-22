2fa8663e9e90e5f7a4a921ee8ca73499b07603d1
Roshan kalamkar <roshankalamkar801@gmail.com>
2023-04-22 22:21:02 +0530

    cal

---
+++ script.js
@@ -0,0 +1,29 @@
+// let sum = 1;
+//  let total = 0;
+// while (sum <= 100){
+//     if(sum % 2 == 0){
+//         total += sum;
+//     }
+//     sum ++;
+// }
+// document.write(total)
+
+
+let odd = 1;
+let all = 0;
+
+while (odd <= 100){
+    if (odd % 2 != 0){
+        all += odd ;
+    }
+    odd ++;
+}
+console.log( all)
+
+
+let counter = 12;
+
+ do{
+     counter ++
+    console.log(counter)
+ } while (counter <= 10)
